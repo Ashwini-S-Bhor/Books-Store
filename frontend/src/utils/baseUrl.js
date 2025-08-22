cff234b5cada0d27d072481a96628462b8b37567
Ashwini-S-Bhor <ashwinishinde9605@gmail.com>
2025-08-22 09:58:51 +0530

    changes made

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,4 +1,6 @@
-const getBaseUrl = ()=>{
+const getBaseUrl = () => {
+    console.log("🔍 Netlify Env Value:", process.env.REACT_APP_API_URL); // Debug log
     return process.env.REACT_APP_API_URL;
-}
+};
+
 export default getBaseUrl;
